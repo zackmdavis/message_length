b5b0b42579ce9466337f6892f3f85eefda15d434
Zack M. Davis <code@zackmdavis.net>
2020-10-12 22:28:07 -0700

    evaluate likelihood; those lower-order parameters aren't a thing

--- src/main.rs
+++ src/main.rs
@@ -18,54 +18,75 @@ struct MarkovTheory {
 }
 
 impl MarkovTheory {
-    fn maximum_likelihood(data: &BitSlice, degree: usize) -> Self {
+    fn likelihood(&self, data: &BitSlice) -> f32 {
+        let mut total = 1.;
+        for window in data.windows(self.degree + 1) {
+            let (observation, tail) = window.split_at(self.degree);
+            let next = tail[0];
+            total *= self.parameters.get(&(observation.to_bitvec(), next)).unwrap();
+        }
+        total
+    }
+
+    fn maximum_likelihood_estimate(data: &BitSlice, degree: usize) -> Self {
         let k = degree.try_into().expect("degree should fit in a u32");
         let mut parameters = HashMap::with_capacity(2usize.pow(k));
-        for prefix_size in 0..degree + 1 {
-            let prefixes = if prefix_size > 0 {
-                (0..prefix_size)
-                    .map(|_| vec![O, I])
-                    .multi_cartesian_product()
-                    .collect::<Vec<_>>()
-            } else {
-                vec![Vec::new()]
-            };
-            for prefix_ in prefixes {
-                // surely there's a better way to initialize BitVecs?!
-                let mut prefix: BitVec<LocalBits, usize> = BitVec::new();
-                prefix.extend(&prefix_);
-                assert_eq!(prefix.len(), prefix_size);
-                let mut chances = 0;
-                let mut zero_continuation = 0;
-                let mut one_continuation = 0;
-                for window in data.windows(prefix_size + 1) {
-                    let (observation, tail) = window.split_at(prefix_size);
-                    let next = tail[0];
-                    if prefix == observation {
-                        chances += 1;
-                        match next {
-                            O => {
-                                zero_continuation += 1;
-                            }
-                            I => {
-                                one_continuation += 1;
-                            }
+        let prefix_size = degree;
+        let prefixes = if prefix_size > 0 {
+            (0..prefix_size)
+                .map(|_| vec![O, I])
+                .multi_cartesian_product()
+                .collect::<Vec<_>>()
+        } else {
+            vec![Vec::new()]
+        };
+        for prefix_ in prefixes {
+            // surely there's a better way to initialize BitVecs?!
+            let mut prefix: BitVec<LocalBits, usize> = BitVec::new();
+            prefix.extend(&prefix_);
+            assert_eq!(prefix.len(), prefix_size);
+            let mut chances = 0;
+            let mut zero_continuation = 0;
+            let mut one_continuation = 0;
+            for window in data.windows(prefix_size + 1) {
+                let (observation, tail) = window.split_at(prefix_size);
+                let next = tail[0];
+                if prefix == observation {
+                    chances += 1;
+                    match next {
+                        O => {
+                            zero_continuation += 1;
+                        }
+                        I => {
+                            one_continuation += 1;
                         }
                     }
                 }
-                assert_eq!(chances, zero_continuation + one_continuation);
-                parameters.insert(
-                    (prefix.clone(), O),
-                    zero_continuation as f32 / chances as f32,
-                );
-                parameters.insert(
-                    (prefix.clone(), I),
-                    one_continuation as f32 / chances as f32,
-                );
             }
+            assert_eq!(chances, zero_continuation + one_continuation);
+            parameters.insert(
+                (prefix.clone(), O),
+                zero_continuation as f32 / chances as f32,
+            );
+            parameters.insert(
+                (prefix.clone(), I),
+                one_continuation as f32 / chances as f32,
+            );
         }
         Self { degree, parameters }
     }
+}
+
+#[derive(Debug)]
+struct ModelScore {
+    fit: f32,
+    complexity: f32
+}
+
+fn evaluate_model(theory: &MarkovTheory, data: &BitSlice) -> ModelScore {
+    let fit = theory.likelihood(data).log2();
+    let complexity = 0.;
+    ModelScore { fit, complexity }
 }
 
 fn main() {
@@ -73,10 +94,10 @@ fn main() {
     data.extend(&[
         O, I, O, I, O, I, O, O, O, O, I, I, I, O, I, O, I, O, I, O, I, O, I, O, I, O, I,
     ]);
-    let zero_theory = MarkovTheory::maximum_likelihood(&data, 0);
-    let one_theory = MarkovTheory::maximum_likelihood(&data, 1);
-    let two_theory = MarkovTheory::maximum_likelihood(&data, 2);
-    let three_theory = MarkovTheory::maximum_likelihood(&data, 3);
+    let zero_theory = MarkovTheory::maximum_likelihood_estimate(&data, 0);
+    let one_theory = MarkovTheory::maximum_likelihood_estimate(&data, 1);
+    let two_theory = MarkovTheory::maximum_likelihood_estimate(&data, 2);
+    let three_theory = MarkovTheory::maximum_likelihood_estimate(&data, 3);
     println!("Hello information theory world!");
     println!("zeroth-order theory: {:?}", zero_theory);
     println!("first-order theory: {:?}", one_theory);
