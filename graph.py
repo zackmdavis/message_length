5bdcc11a6113222c6abc6aaebf3f58b7d865d33f
Zack M. Davis <code@zackmdavis.net>
2020-10-16 00:35:16 -0700

    Markov graphs!

---
+++ graph.py
@@ -0,0 +1,7 @@
+for n in range(1, 4):
+    lines = []
+    for bits in itertools.product(['0', '1'], repeat=n):
+        lines.append("{} -> {}".format(''.join(bits), ''.join(bits[1:] + ('0',))))
+        lines.append("{} -> {}".format(''.join(bits), ''.join(bits[1:] + ('1',))))
+    print('\n'.join(lines))
+    print()
